cacacc9e112b55c159f9f39a94fb76f6ace33ce1
kristenflorey <64163075+kristenflorey@users.noreply.github.com>
2021-01-07 15:49:31 -0700

    set up setter and getter methods

--- tree.py
+++ tree.py
@@ -1,5 +1,50 @@
 class Node:
-    def _init_(self, value):
+    def __init__(self, value):
         self._value = value
         self._parent = None
         self._children = []
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @property
+    def children(self):
+        return self._children
+
+    def add_child(self, node):
+        if node not in self._children:
+            self._children.append(node)
+            node.parent = self
+
+    def remove_child(self, node):
+        if node in self._children:
+            self._children.remove(node)
+            node.parent = None
+
+    @parent.setter
+    def parent(self, node):
+        if self._parent is node:
+            return
+        if self._parent is not None:
+            self._parent.remove_child(self)
+        self._parent = node
+        if node is not None:
+            node.add_child(self)
+
+    # def depth_search(self, value):
+
+
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
+
+# node3.parent = node1
+# node3.parent = node2
+
+# print(node1.children)
+# print(node2.children)
